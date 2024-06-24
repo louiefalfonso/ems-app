ef00d373ed739770685f624fd689a0216d9bb13a
Louie Alfonso <lfriasalfonso@gmail.com>
2024-06-24 22:56:03 +0100

    React SpringBoot FullStack App
    React SpringBoot FullStack App

--- backend/src/main/java/net/fullstacksprinboot/ems_backend/controller/EmployeeController.java
+++ backend/src/main/java/net/fullstacksprinboot/ems_backend/controller/EmployeeController.java
@@ -15,34 +15,41 @@ import java.util.List;
 public class EmployeeController {
     private EmployeeService employeeService;
 
-    // Build ADD Employee Rest API
+    // ADD or POST Employee Rest API
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
-    //Build GET Employee Rest API
+    //GET Employee Rest API
     @GetMapping("{id}")
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
          EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
          return  ResponseEntity.ok(employeeDto);
     }
 
-    //Build Get All Employees Rest API
+    //Get All Employees Rest API
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
         List<EmployeeDto> employees = employeeService.getAllEmployees();
         return ResponseEntity.ok(employees);
     }
 
-    //Build Update Employee Rest API
+    //Update Employee Rest API
     @PutMapping("{id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId,
                                                       @RequestBody EmployeeDto updatedEmployee){
         EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
         return ResponseEntity.ok(employeeDto);
 
+    }
+
+    //Delete Employee Rest API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted Successfully");
 
     }
 
